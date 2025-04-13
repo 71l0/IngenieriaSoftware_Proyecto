cbe6cb61b7da8b8cd56ffda174f53bfa81feff65
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2025-04-12 22:12:10 -0300

    [Parte 4.3] Modificacion en las Vistas

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,33 +3,25 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-from django.http import Http404
 
-def index(request):
+class IndexView(generic.ListView):
+	template_name = "polls/index.html"
+	context_object_name = "latest_question_list"
 
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-	"latest_question_list": latest_question_list,
-    }
-    output = ", ".join([q.question_text for q in latest_question_list])
+	def get_queryset(self):
+		return Question.objects.order_by("-pub_date")[:5]
 
-    return render(request, "polls/index.html", context)
+class DetailView(generic.DetailView):
+	model = Question
+	template_name = "polls/detail.html"
 
-    return HttpResponse(output)
-
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
-
-def results(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, "polls/results.html", {"question": question})
+class ResultsView(generic.DetailView):
+	model = Question
+	template_name = "polls/results.html"
 
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
